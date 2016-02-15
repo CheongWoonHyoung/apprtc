3faab64609b604466b7cef6087eb1ff34f38edc1
DonghyunNa <na_dong_@naver.com>
2016-02-16 05:49:16 +0900

    save

--- AppRTCDemo-master/app/src/main/java/org/appspot/apprtc/Settings.java
+++ AppRTCDemo-master/app/src/main/java/org/appspot/apprtc/Settings.java
@@ -1,7 +1,24 @@
 package org.appspot.apprtc;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 /**
  * Created by cheongwh on 2016. 2. 12..
  */
-public class Settings {
+public class Settings extends MainActivity {
+    @Override
+    protected void onCreate(Bundle SavedInstanceState){
+        super.onCreate(SavedInstanceState);
+        setContentView(R.layout.settings);
+
+        Button btn_home = (Button) findViewById(R.id.btn_home);
+        btn_home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
 }
