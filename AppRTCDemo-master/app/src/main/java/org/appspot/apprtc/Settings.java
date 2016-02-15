e7aef56c674520e33880ab976861786953880e8b
Cheong WoonHyoung <cheongwh@GidiaMacBook.local>
2016-02-16 06:23:48 +0900

    되는거

--- AppRTCDemo-master/app/src/main/java/org/appspot/apprtc/Settings.java
+++ AppRTCDemo-master/app/src/main/java/org/appspot/apprtc/Settings.java
@@ -1,7 +1,17 @@
 package org.appspot.apprtc;
 
+import android.app.Activity;
+import android.os.Bundle;
+
 /**
  * Created by cheongwh on 2016. 2. 12..
  */
-public class Settings {
+public class Settings extends Activity{
+    @Override
+    protected void onCreate(Bundle SavedInstanceState){
+        super.onCreate(SavedInstanceState);
+        setContentView(R.layout.settings);
+
+    }
+
 }
