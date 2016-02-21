ae85ed40c21ae8e34cdb4c6d6c2cfa48958fc634
DonghyunNa <na_dong_@naver.com>
2016-02-21 10:44:39 +0900

    설정 코드 완성

--- AppRTCDemo-master/app/src/main/java/org/appspot/apprtc/Settings.java
+++ AppRTCDemo-master/app/src/main/java/org/appspot/apprtc/Settings.java
@@ -2,11 +2,14 @@ package org.appspot.apprtc;
 
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 
 /**
@@ -24,6 +27,52 @@ public class Settings extends MainActivity {
             @Override
             public void onClick(View v) {
                 finish();
+            }
+        });
+
+        SharedPreferences pref = this.getSharedPreferences(getPackageName(),
+                Activity.MODE_PRIVATE);
+        final SharedPreferences.Editor editor = pref.edit();
+
+        //친구한테 초대받기 설정
+        Switch switch_invite = (Switch)findViewById(R.id.switch_invite);
+        switch_invite.setOnCheckedChangeListener(new Switch.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton cb, boolean isChecking) {
+                if (isChecking) {
+                    editor.putString("invite", "true");
+                    editor.commit();
+                } else{
+                    editor.putString("invite", "false");
+                    editor.commit();
+                }
+            }
+        });
+
+        //모르는 사람으로 부터의 초대 설정
+        Switch switch_new = (Switch)findViewById(R.id.switch_new);
+        switch_new.setOnCheckedChangeListener(new Switch.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton cb, boolean isChecking) {
+                if (isChecking) {
+                    editor.putString("new", "true");
+                    editor.commit();
+                } else{
+                    editor.putString("new", "false");
+                    editor.commit();
+                }
+            }
+        });
+
+        //기타 설정
+        Switch switch_etc = (Switch)findViewById(R.id.switch_etc);
+        switch_etc.setOnCheckedChangeListener(new Switch.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton cb, boolean isChecking) {
+                if (isChecking) {
+                    editor.putString("etc", "true");
+                    editor.commit();
+                } else{
+                    editor.putString("etc", "false");
+                    editor.commit();
+                }
             }
         });
     }
